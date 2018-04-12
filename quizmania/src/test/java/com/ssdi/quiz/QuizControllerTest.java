0f036009e4bf1382be3bc8ec62ea212ca7684bd1
Jay <jaychandrakamineni@gmail.com>
2018-04-12 00:31:59 -0400

    Few Test cases, front end with no errors

---
+++ quizmania/src/test/java/com/ssdi/quiz/QuizControllerTest.java
@@ -0,0 +1,65 @@
+package com.ssdi.quiz;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssdi.context.ContextLoaderTest;
+
+import com.ssdi.quiz.QuizController;
+import com.ssdi.user.examiner.*;
+import com.ssdi.quiz.*;
+
+public class QuizControllerTest extends ContextLoaderTest{
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@MockBean
+	private QuizController controller;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void testViewQuiz() throws Exception {
+		Quiz quiz = new Quiz();
+	    quiz.setTitle("Quant");
+
+	       List<Quiz> allQuizzes = singletonList(quiz);
+
+	       given(controller.getAllQuiz()).willReturn(allQuizzes);
+		
+		this.mockMvc.perform(get("http://localhost:8080/quizmania/examiner/viewQuiz").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].title", is(quiz.getTitle())));
+	}
+	
+}
---
+++ quizmania/src/test/java/com/ssdi/quiz/QuizRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.ssdi.quiz;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssdi.context.ContextLoaderTest;
+import com.ssdi.quiz.Quiz;
+import com.ssdi.quiz.QuizRepository;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class QuizRepositoryTest extends ContextLoaderTest{
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@Autowired
+    private TestEntityManager entityManager;
+ 
+    @Autowired
+    private QuizRepository repository;
+    
+    @Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void setQuizStatusTest() {
+		//given
+		Quiz quiz= new Quiz();
+		quiz.setStatus("pending");
+		quiz.setQuizID(1);
+		entityManager.persist(quiz);
+	    entityManager.flush();
+	    
+	    //when
+	    repository.setQuizStatus("Approved", 1);
+	    
+	    //then
+	    assertThat(quiz.getStatus())
+	      .isEqualTo("Approved");
+	}
+
+}
